1e4369a6ce44d5dc3616f99de918ad467d662dde
SidathDabare <sidath2007@gmail.com>
2022-10-24 23:25:07 +0200

    add whitelist

--- src/server.js
+++ src/server.js
@@ -24,7 +24,29 @@ const server = express()
 const port = process.env.PORT || 3001
 
 // ********************************* MIDDLEWARES *****************************
-server.use(cors())
+// server.use(cors())
+const whitelist = [process.env.FE_DEV_URL, process.env.FE_PROD_URL]
+server.use(
+  cors({
+    origin: (origin, corsNext) => {
+      // If you want to connect FE to this BE you must use cors middleware
+      console.log("ORIGIN: ", origin)
+
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        // if origin is in the whitelist we can move next
+        corsNext(null, true)
+      } else {
+        // if origin is NOT in the whitelist --> trigger an error
+        corsNext(
+          createHttpError(
+            400,
+            `Cors Error! Your origin ${origin} is not in the list!`
+          )
+        )
+      }
+    },
+  })
+)
 server.use(express.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 
