a7afba098b001cf3e574ab3bda446252ee6dc0f6
SidathDabare <sidath2007@gmail.com>
2023-02-07 18:33:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -30,30 +30,23 @@ const publicFolderPath = join(process.cwd(), "./pdf")
 
 // ********************************* MIDDLEWARES *****************************
 // server.use(cors())
-// const whitelist = [process.env.FE_PROD_URL, process.env.FE_DEV_URL]
-// server.use(cors({ origin: [process.env.FE_PROD_URL, process.env.FE_DEV_URL] }))
-// server.use(
-//   cors({
-//     origin: (origin, corsNext) => {
-//       console.log("ORIGIN: ", origin)
-
-//       if (!origin || whitelist.indexOf(origin) !== -1) {
-//         corsNext(null, true)
-//       } else {
-//         corsNext(
-//           createHttpError(
-//             400,
-//             `Cors Error! Your origin ${origin} is not in the list!`
-//           )
-//         )
-//       }
-//     },
-//   })
-// )
-
+const whitelist = [process.env.FE_PROD_URL, process.env.FE_DEV_URL]
 server.use(
   cors({
-    origin: "*",
+    origin: (origin, corsNext) => {
+      console.log("ORIGIN: ", origin)
+
+      if (!origin || whitelist.indexOf(origin) !== -1) {
+        corsNext(null, true)
+      } else {
+        corsNext(
+          createHttpError(
+            400,
+            `Cors Error! Your origin ${origin} is not in the list!`
+          )
+        )
+      }
+    },
   })
 )
 server.use(express.static(publicFolderPath))
